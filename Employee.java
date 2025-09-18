265f13d4d8f255f8ff976d587522623543fe4854
raymond tonio <rraymondtonio@gmail.com>
2025-09-18 13:49:07 -0400

    ORIGINAL ANSWERS TRANSCRIBED

--- Employee.java
+++ Employee.java
@@ -29,22 +29,35 @@ public class Employee{
     /**
      * Returns true is an employee is above 16 years old
      */
-    public boolean canDrive(){
-        int age = calculateAge(2025);
-             ;// to be completed
+    public int canDrive(){
+        if (age >= 16)
+        System.out.println("Can Drive");
+        else 
+        System.out.println("16"-"age"+"years to wait to start driving");
     }
 
     /*
      * Returns the net pay for the outstanding unpaid hours
      */
     private double calculatePay(){
-        // to be completed
+        // 1) Set Value to unpaidhours to 0.0
+        // 2) Set Value to hourlyWage to 10 dollars per hour(dph)
+        // 3) Set Value to taxes and deductions rate to 30%
+        // 4) Increment unpaidHours by 10
+        // 5) Set total salary to unpaidhours * hourlyWage
+        // 6) Taxes and deductions = total salary * taxes and deduction rate
+        // 7) Net pay = total salary - taxes and deductions
+        // 8) Print Net pay
     }
 
     /*
      * Output the payment record and resets unpaid hours
      */
     public void paySalary(){
-        // to be completed
+       paySalary(5000); // regarding question 11
+       
+       if ( String fullname = "John Deere")
+       System.out.println("John Deere has received a wire transfer of 100 CAD")
+       udpate unpaidhours;
     }
 }
